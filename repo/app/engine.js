c2d3df15aa969266167bd0141f19f30cd9f6778b
cwar-tech <c.lang.softwaretest@gmail.com>
2026-05-24 10:45:23 +0200

    engine.js aktualisieren

--- repo/app/engine.js
+++ repo/app/engine.js
@@ -1,1 +1,29 @@
-// Foundation Engine
+// ==================================================
+// app/engine.js
+// ==================================================
+
+import shipsData from "./ships.json" assert { type: "json" };
+
+import combatInput from
+  "./DATA/COMBAT/API/TESTDATA/COMBAT-INPUT-001.json"
+  assert { type: "json" };
+
+import { resolveFleet }
+  from "./combat/resolveFleet.js";
+
+
+const attackerFleet = resolveFleet(
+  combatInput.attacker,
+  shipsData
+);
+
+const defenderFleet = resolveFleet(
+  combatInput.defender,
+  shipsData
+);
+
+console.log("ATTACKER FLEET");
+console.log(attackerFleet);
+
+console.log("DEFENDER FLEET");
+console.log(defenderFleet);
