5929681fc872afa0c4307adde8c77e5d9287175e
cwar-tech <c.lang.softwaretest@gmail.com>
2026-06-11 11:03:07 +0200

    Create ValidationData.js

---
+++ report/dto/ValidationData.js
@@ -0,0 +1,34 @@
+// ==================================================
+// report/dto/ValidationData.js
+// ==================================================
+
+class ValidationData {
+
+    constructor({
+
+        expectedAttacks,
+
+        executedAttacks,
+
+        allUnitsAttacked,
+
+        duplicateAttacks
+
+    } = {}) {
+
+        this.expectedAttacks =
+            expectedAttacks;
+
+        this.executedAttacks =
+            executedAttacks;
+
+        this.allUnitsAttacked =
+            allUnitsAttacked;
+
+        this.duplicateAttacks =
+            duplicateAttacks;
+    }
+}
+
+export default
+    ValidationData;
