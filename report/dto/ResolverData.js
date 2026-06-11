73cae0f07d44fca6265016dceec54afac34d0500
cwar-tech <c.lang.softwaretest@gmail.com>
2026-06-11 06:58:54 +0200

    ResolverData.js erstellen

---
+++ report/dto/ResolverData.js
@@ -0,0 +1,19 @@
+// ==================================================
+// report/dto/ResolverData.js
+// ==================================================
+
+class ResolverData {
+
+    constructor({
+
+        combatRounds
+
+    } = {}) {
+
+        this.combatRounds =
+            combatRounds;
+    }
+}
+
+export default
+    ResolverData;
